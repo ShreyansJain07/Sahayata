a242ab0fb1c61f653ff5307a77e8517a6e5f62af
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2024-01-17 15:46:33 +0530

    First

--- cra-frontend/src/App.js
+++ cra-frontend/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import Signup from './components/SignUp';
-import BrowseTalent from './screens/BrowseTalent';
-import ResumeBuilder from './screens/ResumeBuilder';
+import "./App.css";
+import Signup from "./components/SignUp";
+import BrowseTalent from "./screens/BrowseTalent";
+import ResumeBuilder from "./screens/ResumeBuilder";
+import VirtualAssistant from "./screens/VirtualAssistant";
 
 function App() {
   return (
-    
-      <div className="App">
-        <Routes>
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/browse" element={<BrowseTalent/>} />
-          <Route path="/resumebuilder" element={<ResumeBuilder/>} />
-
-        </Routes>
-      </div>
-    
+    <div className="App">
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/browse" element={<BrowseTalent />} />
+        <Route path="/virtualassistant" element={<VirtualAssistant />} />
+        <Route path="/resumebuilder" element={<ResumeBuilder />} />
+      </Routes>
+    </div>
   );
 }
 
