b3003ec2020434c2029a29c4e2375f04c2115bc6
Shreyans jain <shreyansjain1603@gmail.com>
2024-01-17 16:33:26 +0530

    commit

--- cra-frontend/src/App.js
+++ cra-frontend/src/App.js
@@ -1,23 +1,76 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import './App.css';
-import Signup from './components/SignUp';
-import BrowseTalent from './screens/BrowseTalent';
-import ResumeBuilder from './screens/ResumeBuilder';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppContext from "./AppContext";
+import "./App.css";
+import Signup from "./components/SignUp";
+import BrowseTalent from "./screens/BrowseTalent";
+import ResumeBuilder from "./screens/ResumeBuilder";
+import questionsArray from "./constants/questionsArray";
 
 function App() {
+  let [questions, setQuestions] = useState([]);
+  let [answers, setAnswers] = useState([]);
+  let [questionAnswer, setQuestionAnswer] = useState({});
+  let [questionCompleted, setQuestionCompleted] = useState(false);
+
+  useEffect(() => {
+    setQuestions(questionsArray);
+    setQuestionAnswer(questionsArray[0]);
+  }, []);
+
+  let handleChangeInput = (e) => {
+    setQuestionAnswer({
+      ...questionAnswer,
+      answer: e.target.value,
+    });
+  };
+
+  let nextQuestion = (e) => {
+    e.preventDefault();
+    questions.map((question) => {
+      if (question.resumeFieldId == questionAnswer.resumeFieldId) {
+        setAnswers([
+          ...answers,
+          { ...question, answer: questionAnswer.answer },
+        ]);
+      }
+    });
+
+    questions.map((qa, index) => {
+      if (index <= questions.length) {
+        if (qa.resumeFieldId === questionAnswer.resumeFieldId) {
+          setQuestionAnswer(questions[index + 1]);
+        }
+      } else {
+        setQuestionCompleted(true);
+      }
+    });
+  };
   return (
-    
+    <>
+    <AppContext.Provider
+      value={{
+        state: {
+          questionAnswer,
+          questionCompleted,
+          questions,
+          answers,
+        },
+        function: {
+          handleChangeInput: handleChangeInput,
+          nextQuestion: nextQuestion,
+        },
+      }}
+    >
       <div className="App">
         <Routes>
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/browse" element={<BrowseTalent/>} />
-          <Route path="/resumebuilder" element={<ResumeBuilder/>} />
-
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/browse" element={<BrowseTalent />} />
+          <Route path="/resumebuilder" element={<ResumeBuilder />} />
         </Routes>
       </div>
-    
+    </AppContext.Provider>
+    </>
   );
 }
 
