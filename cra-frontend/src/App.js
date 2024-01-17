f71eb7242f0d0af104b795292bc6b4616bcb387a
varun jajoo <varun.jajoo18@gmail.com>
2024-01-17 14:46:28 +0530

    commit

--- cra-frontend/src/App.js
+++ cra-frontend/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import Signup from './components/SignUp';
 import BrowseTalent from './screens/BrowseTalent';
+import ResumeBuilder from './screens/ResumeBuilder';
 
 function App() {
   return (
-    <div className="App">
-      <Signup />
-      <BrowseTalent />
-    </div>
+    
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/browse" element={<BrowseTalent/>} />
+          <Route path="/resumebuilder" element={<ResumeBuilder/>} />
+
+        </Routes>
+      </div>
+    
   );
 }
 
