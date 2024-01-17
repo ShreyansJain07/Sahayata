309b348aa81deccdc409aea7849c121ff2cec303
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2024-01-17 14:30:33 +0530

    First

--- cra-frontend/src/App.js
+++ cra-frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/SignUp';
+import BrowseTalent from './screens/BrowseTalent';
 
 function App() {
   return (
     <div className="App">
       <Signup />
+      <BrowseTalent />
     </div>
   );
 }
--- cra-frontend/src/screens/BrowseTalent.jsx
+++ cra-frontend/src/screens/BrowseTalent.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Button,
+  Input,
+} from "@chakra-ui/react";
 
 const BrowseTalent = () => {
-  return <div>BrowseTalent</div>;
+  const [availableCandidates, setAvailableCandidates] = useState([
+    { name: "Rishab", skills: "Front-End Dev" },
+    { name: "Shreyans", skills: "Front-End Dev" },
+  ]);
+  return (
+    <div style={{ backgroundColor: "#d3d3d3" }}>
+      <hr />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          padding: "1rem",
+        }}
+      >
+        <div style={{ fontWeight: "bold", fontSize: "1.5rem" }}>
+          Browse Talent
+        </div>
+        <div>
+          <Button
+            variant="outline"
+            style={{
+              fontSize: "0.7rem",
+              padding: "0.5rem",
+              paddingTop: "0",
+              paddingBottom: "0",
+            }}
+            color="#2234da"
+          >
+            Invite To Job
+          </Button>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        {availableCandidates?.map((candidate) => {
+          return (
+            <div style={{ padding: "1rem", paddingBottom: "0" }}>
+              <Card
+                direction={{ base: "column", sm: "row" }}
+                overflow="hidden"
+                variant="outline"
+              >
+                <Image
+                  objectFit="cover"
+                  maxW={{ base: "100%", sm: "200px" }}
+                  src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+                  alt="Caffe Latte"
+                />
+                <Stack>
+                  <CardBody style={{ textAlign: "left" }}>
+                    <Heading size="md">{candidate?.name}</Heading>
+                    <Text py="2">
+                      Caffè latte is a coffee beverage of Italian origin made
+                      with espresso and steamed milk.
+                    </Text>
+                    <Text pb="2">Speaks: English, Hindi</Text>
+                    <Text pb="2">Skills: {candidate?.skills}</Text>
+                  </CardBody>
+                  <CardFooter>
+                    <Button variant="outline" color="#ff5045">
+                      Invite To Job
+                    </Button>
+                  </CardFooter>
+                </Stack>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BrowseTalent;
