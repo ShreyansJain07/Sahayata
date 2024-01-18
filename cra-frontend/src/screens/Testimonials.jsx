5ac29ad52de7114d2e113bf3ee4ebe3600e7037a
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2024-01-18 17:12:01 +0530

    First

---
+++ cra-frontend/src/screens/Testimonials.jsx
@@ -0,0 +1,280 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f3f4f6",
+        padding: "2rem",
+        // marginLeft: "-2rem",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          fontWeight: 600,
+          textAlign: "center",
+          marginBottom: "1.9rem",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          //   marginBottom: "0.5rem",
+        }}
+      >
+        People loving using{" "}
+        <h1
+          style={{
+            color: "#ffcf36",
+            fontSize: "2.0rem",
+            fontWeight: "bolder",
+            marginLeft: "0.5rem",
+          }}
+        >
+          सह<span style={{ color: "#3261ff" }}>AI</span>ता
+        </h1>
+      </div>
+      <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1rem",
+            padding: "1rem",
+            fontWeight: 500,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>
+            "Finding a job that values my skills and accommodates my visual
+            impairment used to be a challenge. Thanks to this platform, I landed
+            a fulfilling job in software development. The inclusive job listings
+            and supportive community made all the difference!"
+          </div>
+          <div
+            style={{
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+            }}
+          >
+            <img
+              src="https://topmate.io/_next/image?url=%2Fimages%2Fhomepage%2Ftestimonials%2Fdr_joerg_storm.webp&w=48&q=75"
+              alt="Profile photo"
+              style={{ borderRadius: "1rem" }}
+            />
+            <div>Varun Jajoo</div>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1rem",
+            padding: "1rem",
+            fontWeight: 500,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>
+            "I always felt my hearing impairment was a barrier to finding the
+            right job. This website not only connected me with inclusive
+            employers but also provided resources for effective communication.
+            Now, I'm thriving in my marketing career!"
+          </div>
+          <div
+            style={{
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+            }}
+          >
+            <img
+              src="https://topmate.io/_next/image?url=%2Fimages%2Fhomepage%2Ftestimonials%2Fdr_joerg_storm.webp&w=48&q=75"
+              alt="Profile photo"
+              style={{ borderRadius: "1rem" }}
+            />
+            <div>Varun Jajoo</div>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1rem",
+            padding: "1rem",
+            fontWeight: 500,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>
+            "As a wheelchair user, I faced obstacles in my job search. This
+            platform not only offered accessible job listings but also empowered
+            me with resources on workplace accessibility. I am now managing
+            projects and making a difference!"
+          </div>
+          <div
+            style={{
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+            }}
+          >
+            <img
+              src="https://topmate.io/_next/image?url=%2Fimages%2Fhomepage%2Ftestimonials%2Fdr_joerg_storm.webp&w=48&q=75"
+              alt="Profile photo"
+              style={{ borderRadius: "1rem" }}
+            />
+            <div>Varun Jajoo</div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1rem",
+          marginTop: "1rem",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1rem",
+            padding: "1rem",
+            fontWeight: 500,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>
+            "Being neurodivergent made job hunting daunting. This website not
+            only celebrated neurodiversity but also connected me with employers
+            who value diverse perspectives. I'm now expressing my creativity as
+            a graphic designer!"
+          </div>
+          <div
+            style={{
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+            }}
+          >
+            <img
+              src="https://topmate.io/_next/image?url=%2Fimages%2Fhomepage%2Ftestimonials%2Fdr_joerg_storm.webp&w=48&q=75"
+              alt="Profile photo"
+              style={{ borderRadius: "1rem" }}
+            />
+            <div>Varun Jajoo</div>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1rem",
+            padding: "1rem",
+            fontWeight: 500,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>
+            "Transitioning from the military with PTSD posed challenges. This
+            platform understood my unique situation and connected me with
+            employers who prioritize mental health support. Now, I'm excelling
+            in IT support with a supportive team."
+          </div>
+          <div
+            style={{
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+            }}
+          >
+            <img
+              src="https://topmate.io/_next/image?url=%2Fimages%2Fhomepage%2Ftestimonials%2Fdr_joerg_storm.webp&w=48&q=75"
+              alt="Profile photo"
+              style={{ borderRadius: "1rem" }}
+            />
+            <div>Varun Jajoo</div>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1rem",
+            padding: "1rem",
+            fontWeight: 500,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>
+            "Mobility impairment made traditional job search frustrating. This
+            website not only listed accessible job opportunities but also
+            provided guidance on workplace accommodations. I'm now thriving as
+            an administrative assistant!"
+          </div>
+          <div
+            style={{
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+            }}
+          >
+            <img
+              src="https://topmate.io/_next/image?url=%2Fimages%2Fhomepage%2Ftestimonials%2Fdr_joerg_storm.webp&w=48&q=75"
+              alt="Profile photo"
+              style={{ borderRadius: "1rem" }}
+            />
+            <div>Varun Jajoo</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
