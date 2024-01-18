82f85f3ceb8ffa87d9ef2bfe8b35e8a56d6f6f84
Ritojnan <ritojnanm@gmail.com>
2024-01-19 01:01:47 +0530

    readaloud button added

--- cra-frontend/src/components/ReadAloud.jsx
+++ cra-frontend/src/components/ReadAloud.jsx
@@ -2,86 +2,23 @@ import React, { useState, useEffect } from "react";
 import {
   Box,
   IconButton,
-  Button,
-  Text,
-  ModalFooter,
-  Icon,
-  SimpleGrid,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerHeader,
-  DrawerBody,
-  useDisclosure,
 } from "@chakra-ui/react";
 
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-import useClipboard from "react-use-clipboard";
 import {
   FaMicrophoneAlt,
   FaMicrophoneSlash,
   FaMicrophone,
 } from "react-icons/fa";
+import { Speaker } from "@material-ui/icons";
+import { HiSpeakerWave,HiSpeakerXMark } from "react-icons/hi2";
 
-const LanguageButtons = () => {
-  const languages = [
-    { name: "Hindi", code: "hi" },
-    { name: "Tamil", code: "ta" },
-    { name: "Telugu", code: "te" },
-    { name: "Bengali", code: "bn" },
-    // Add more languages as needed
-  ];
 
-  return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 4 }} spacing={4} width="full">
-      {languages.map((language) => (
-        <Button
-          key={language.code}
-          width="full"
-          colorScheme="teal"
-          size={"lg"}
-          onFocus={() =>
-            SpeechRecognition.startListening({ language: language.code })
-          }
-        >
-          <Text fontSize="lg">{language.name}</Text>
-        </Button>
-      ))}
-    </SimpleGrid>
-  );
-};
+const ReadAloud = ({value,handleChangeInput}) => {
+  const {  browserSupportsSpeechRecognition } = useSpeechRecognition();
 
-const VoiceButton = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isListening, setIsListening] = useState(false);
-  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-
-  const startListening = () => {
-    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
-    setIsListening(true);
-    console.log(transcript)
-      if (transcript.includes("hello")) {
-        stopListening();
-    
-        const initialSpeech = "Voice Assistant, how may I help you today?";
-        const initialUtterance = new SpeechSynthesisUtterance(initialSpeech);
-    
-        initialUtterance.onend = () => {
-          // Speech synthesis has ended, set isListening to true
-          // startListening();
-          // setIsListening(true);
-        };
-    
-        window.speechSynthesis.speak(initialUtterance);
-      }    
-  };
-
-  const stopListening = () => {
-    SpeechRecognition.stopListening();
-    setIsListening(false);
-  };
 
 
   
@@ -93,57 +30,22 @@ const VoiceButton = () => {
   }
 
   return (
-    <Box color={"teal.800"} aria-label="Voice Assistant">
+    <Box color={ value ? "red.800" : "teal.800"} aria-label="Read Aloud">
       <IconButton
-        icon={<FaMicrophone />}
-        aria-label="Voice Button"
+        icon={value ? <HiSpeakerXMark /> : <HiSpeakerWave />}
+        aria-label="Read Aloud"
+        colorScheme={ value ? "red" : "teal"}  
         onClick={()=>{
-            const initialSpeech = "Voice Assistant How may I help you today?";
+            const initialSpeech = value?"Read Aloud Stopped":"Read Aloud Started";
             const initialUtterance = new SpeechSynthesisUtterance(initialSpeech);
             window.speechSynthesis.speak(initialUtterance);
-          
-          onOpen()}}
+            handleChangeInput(!value);
+          }}
         size="lg"
-        colorScheme="teal"
         aria-labelledby="voice-assistant"
       />
-
-      <Drawer isOpen={isOpen} placement="bottom" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader>Voice Assistant</DrawerHeader>
-          <DrawerBody>
-            {/* Your content here */}
-            <LanguageButtons />
-            <Text>{transcript}</Text>
-          </DrawerBody>
-          <ModalFooter>
-            {isListening ? (
-              <Button
-                colorScheme="red"
-                aria-label="Stop Listening"
-                leftIcon={<Icon as={FaMicrophoneSlash} />}
-                width={"full"}
-                onClick={stopListening}
-              >
-                Stop Listening
-              </Button>
-            ) : (
-              <Button
-                style={{ backgroundColor: "#2234da", color: "white" }}
-                aria-label="Start Listening"
-                leftIcon={<Icon as={FaMicrophoneAlt} />}
-                width={"full"}
-                onClick={startListening}
-              >
-                Start Listening
-              </Button>
-            )}
-          </ModalFooter>
-        </DrawerContent>
-      </Drawer>
     </Box>
   );
 };
 
-export default VoiceButton;
+export default ReadAloud;
